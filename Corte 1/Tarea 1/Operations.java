3dedebb54603cbd52daab601465c55e6e5f147da
Rikutaa07 <r.yanquen07@gmail.com>
2025-04-13 19:06:13 -0500

    Create Operations.java

---
+++ Corte 1/Tarea 1/Operations.java
@@ -0,0 +1,26 @@
+public class Operations {
+    
+    public int a,b,r;
+   // Métodos para operaciones matemáticas
+    public int sumar(int a, int b) {
+        r = a + b;
+        return r;
+    }
+
+    public int restar(int a, int b) {
+       r = a - b;
+        return r;
+           
+    }
+
+    public int multiplicar(int a, int b) {
+        r = a * b;
+        return r;    
+    }
+    public int potencia (int base, int exponente){
+        
+        r = exponente == 0 ? 1: base*potencia(base,exponente-1);
+        return r;
+    }
+  
+}
